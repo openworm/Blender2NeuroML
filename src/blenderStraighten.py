e8dc5ba09ea9da453c2a5a9c4023c81ff91c5496
Petr Baudis <pasky@ucw.cz>
2013-10-05 01:11:11 +0200

    blenderStraighten.py: Perform the actual straightening transformation

--- src/blenderStraighten.py
+++ src/blenderStraighten.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import bpy
+import mathutils
 
 # This is required in order for `import numpy` to succeed on Debian.
 import sys
 sys.path.append("/usr/lib/python3/dist-packages")
 
+import math
 import numpy
 
 def bezier_poly(p, t):
@@ -49,23 +51,129 @@ def bezier_spline_pointset(base, bzspline):
     return numpy.concatenate(pointsets)
 
 
+def pointset_mileage(pointset):
+    """
+    Distance (approximated by straight lines) from the spine beginning,
+    to be used as the Y coordinate of straightened worm.
+    """
+    # @distances lists pointwise distances between successive pairs
+    # of points
+    distances = numpy.sqrt(numpy.sum((pointset[1:] - pointset[:-1]) ** 2, 1))
+
+    mileage = 0.
+    mileageset = [0.]
+    # Simple accumulation
+    for i in range(len(distances)):
+        mileage += distances[i]
+        mileageset.append(mileage)
+    return mileageset
+
+
 def pointset_to_objects(pointset):
     """
     This function is just for visualization of intermediate data
     (i.e. debugging).
     """
     for point in pointset:
-        print(point)
+        # print(point)
         mesh = bpy.data.meshes.new("b" + str(point))
         ob = bpy.data.objects.new("b" + str(point), mesh)
         ob.location = point
         bpy.context.scene.objects.link(ob)
 
+
+def nearest_point_pair(pointset, coord):
+    """
+    Return the two nearest points in pointset, leftmost first.
+    """
+    # XXX: Extremely naive, compute distances to all points and sort
+    distances = numpy.sqrt((pointset[:,0] - coord[0]) ** 2 + (pointset[:,1] - coord[1]) ** 2 + (pointset[:,2] - coord[2]) ** 2)
+    dist_i = sorted(zip(distances, range(len(distances))), key = lambda o: o[0])
+    i, j = dist_i[0][1], dist_i[1][1]
+    if i > j:
+        i, j = j, i
+    return (i, j)
+
+def transform_coord(pointset, newyset, coord):
+    """
+    Reinterpolate the coordinate of each vertex of each object
+    by using pointset as the axis guide.
+    """
+    i, j = nearest_point_pair(pointset, coord)
+    #print(i, j)
+
+    # We are interested in the nearest point @k on line [i,j];
+    # this point can be then used to interpolate y from newyset,
+    # distance from @k to @coord is then z.
+
+    # Note that pointset[.], coord [0] is the x-coordinate which
+    # we are not recomputing (the worm is already straight along
+    # the x axis). The other indices are therefore used shifted
+    # accordingly.
+
+    d_ij = pointset[j] - pointset[i]
+    v = numpy.array([d_ij[1], d_ij[2]])
+    w = numpy.array([-v[1], v[0]]) # w is perpendicular to v; v always points rightwards
+    w = w / numpy.sqrt(numpy.dot(w, w)) # normalize w
+    #print('v', v, 'w', w)
+
+    # Find an intersection between lines [i] + s*v and coord + t*w:
+    # [i]0 + s*v0 = coord0 + t*w0      [i]1 + s*v1 = coord1 + t*w1
+    # t = ([i]0 + s*v0 - coord0) / w0  [i]1 + s*v1 = coord1 + ([i]0 + s*v0 - coord0) * w1/w0
+    # => [i]1 + s*v1 = coord1 + [i]0*w1/w0 + s*v0*w1/w0 - coord0*w1/w0
+    # => s*v1 - s*v0*w1/w0 = coord1 + [i]0*w1/w0 - [i]1 - coord0*w1/w0
+    # => s = (coord1 + [i]0*w1/w0 - [i]1 - coord0*w1/w0) / (v1 - v0*w1/w0)
+    s = (coord[2] + pointset[i][1]*w[1]/w[0] - pointset[i][2] - coord[1]*w[1]/w[0]) / (v[1] - v[0]*w[1]/w[0])
+    t = (pointset[i][1] + s*v[0] - coord[1]) / w[0]
+
+    # s is in [0,1], usable for interpolation
+    # t is unit vector, so we can directly use it as z (distance from spine)
+    y = (1. - s) * newyset[i] + s * newyset[j]
+    z = t
+    return mathutils.Vector((coord[0], y, z))
+
+def transform_objects(pointset, newyset, objectset):
+    """
+    Reinterpolate the coordinate of each vertex of each object
+    by using pointset as the axis guide.
+    """
+
+    for o in objectset:
+        # Make sure the object vertices correspond to true location
+        bpy.context.scene.objects.active = o
+        o.select = True
+        bpy.ops.object.transform_apply(scale=True, rotation=True)
+        o.select = False
+
+        print(o)
+        for i in range(len(o.data.vertices)):
+            v = o.data.vertices[i]
+            coord_in = v.co + o.location
+            #print(':-', v.co, '=>', v.co + o.location, '=', coord_in, 'loc', o.location)
+
+            coord_out = transform_coord(pointset, newyset, coord_in)
+            o.data.vertices[i].co = coord_out - o.location
+            #print(':+', coord_out, '=>', coord_out - o.location, '=', o.data.vertices[i].co, 'loc', o.location)
+
+        o.data.update()
+
+
 if __name__ == '__main__':
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    # Convert the spine to a dense sequence of sampled points on the spine
     spine = bpy.data.objects['Center spine for worm model']
     bzspline = spine.data.splines[0]
     pointset = bezier_spline_pointset(numpy.array(spine.location), bzspline)
 
-    pointset_to_objects(pointset)
+    # Compute Y coordinates in straightened worm corresponding to the pointset
+    mileageset = pointset_mileage(pointset)
+    newybase = mileageset[-1] / 2.
+    newyset = list(map(lambda x: x - newybase, mileageset))
 
+    # Debug stop along the way:
+    #pointset_to_objects(pointset)
     #bpy.ops.wm.save_mainfile(check_existing = False)
+
+    # Transform mesh coordinates by interpolation
+    transform_objects(pointset, newyset, filter(lambda o: o.type == "MESH", bpy.data.objects))
