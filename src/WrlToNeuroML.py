2ffb68ccaede0e830a0a08b367363452363cabc6
TC <96085470+TC-Wheel@users.noreply.github.com>
2022-01-24 16:58:05 -0700

    Update WrlToNeuroML.py
    trying to import neuroml module.

--- src/WrlToNeuroML.py
+++ src/WrlToNeuroML.py
@@ -7,6 +7,7 @@ Created on 03.06.2011
 import re, sys, os
 
 import numpy
+import neuroml
 from NeuroMlEntity.Point import Point
 from NeuroMlEntity.Constants import *
 from NeuroMlParser.NeuroMlWriter import NeuroMlWriter
