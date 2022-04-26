5baf95021ad8c31ec6efad7b92b3a8224b318181
TC <96085470+TC-Wheel@users.noreply.github.com>
2022-04-25 19:28:43 -0600

    Update runExternalScript.py
    added description and fixed bugs

--- src/NeuronBlenderImaging/runExternalScript.py
+++ src/NeuronBlenderImaging/runExternalScript.py
@@ -1,5 +1,13 @@
-#This is a template script which you can just reload to execute the script you're currently editing. 
-#Just change the filename and path name.
+"""This is an edited template script from Blender that I find useful
+in writing code and debugging.
+How to use:
+1. Load this script in Blender (see installDependencies for this)
+2. Type in your own script and path relative to the location of the Blender file
+(be aware the the CWD of Blender is inside the Blender install, 
+not where you are running the .blend file)
+3. Push the play button (Hotkey: ALT+P) to run the specified script
+4. Cry (I'm sorry you have to script for Blender)
+"""
 
 import bpy
 import os
@@ -12,9 +20,9 @@ blendFileDir = os.path.dirname(os.path.realpath(__file__))
 scriptPath = os.path.abspath(os.path.join(blendFileDir, "..", "..", "NeuronBlenderImaging", filename))
 
 #commenent this out if you want to execute a script in the same directory as the blender file.
-#scriptPath = os.path.join(os.path.dirname(bpy.data.filepath), filename)
+#scriptPath = os.path.join(os.path.dirname(bpy.data.scriptPath), filename)
 
 
 global_namespace = {"__file__": scriptPath, "__name__": "__main__"}
-with open(filepath, 'rb') as file:
-    exec(compile(file.read(), filepath, 'exec'), global_namespace)
+with open(scriptPath, 'rb') as file:
+    exec(compile(file.read(), scriptPath, 'exec'), global_namespace)
