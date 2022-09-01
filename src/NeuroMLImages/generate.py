e7d9f75cf83d55c16394d58f5e5344a9658dd00b
Padraig Gleeson <p.gleeson@gmail.com>
2022-09-01 11:07:24 +0100

    Restructure readme

--- src/NeuroMLImages/generate.py
+++ src/NeuroMLImages/generate.py
@@ -3,11 +3,10 @@ from pyneuroml.pynml import nml2_to_svg
 import shutil
 
 
-
 all_cells = ['ADAL','AIZL','CEPDR','DVC','IL1DR','RIFL','PVCR','SMBVR','URYDR']
 #all_cells = ['AIZL']
 
-info = '## Blender and NeuroML images\n\n'
+info = '## Blender and corresponding NeuroML images\n\n'
 info2 = ''
 
 for cell in all_cells:
@@ -37,32 +36,36 @@ for cell in all_cells:
                 save_to_file=p2d_file,
                 square=True)
 
-    info += ' [%s](#%s) '%(cell,cell.lower())
+    info += ' [%s](#%s) | '%(cell,cell.lower())
 
     info2 += '### %s\n'%cell
 
     height=250
-    info2 += '<table border="0">\n'
-    info2 += '   <tr><td>Images generated from NeuroML file <a href="../Output/C.Elegans_%s.nml">C.Elegans_%s.nml</a></td>\n'%(cell,cell)
+    info2 += '<table border="0">\n  <tr>\n'
+    info2 += '    <td>Images generated from original Blender file</td>\n'
+    info2 += '    <td>Images generated from NeuroML file <a href="../Output/C.Elegans_%s.nml">C.Elegans_%s.nml</a></td>\n  </tr>\n'%(cell,cell)
     #info2 += '   <td>Images generated from NeuroML file <a href="../Output/C.Elegans_%s.nml">C.Elegans_%s.nml</a></td>\n'%(cell,cell)
-    info2 += '   <td>Images generated from Blender</td></tr>\n   <tr>\n'
     #info2 += '   <td><img src="C.Elegans_%s.svg" alt="%s" height="600"/></td>\n'%(cell,cell)
 
-    info2 += '   <td>\n'
+    info2 += '  <tr>\n'
+
+    info2 += '    <td>\n      <img src="../NeuronBlenderImaging/NeuronScreenshots/%s_side.png" alt="%s" height="%s"/><br/>\n'%(cell,cell, height)
+    info2 += '       <img src="../NeuronBlenderImaging/NeuronScreenshots/%s_front.png" alt="%s" height="%s"/><br/>\n'%(cell,cell,height)
+    info2 += '       <img src="../NeuronBlenderImaging/NeuronScreenshots/%s_top.png" alt="%s" height="%s"/>\n    </td>\n'%(cell,cell, height)
+
+    info2 += '    <td>\n'
     for plane in planes:
         info2 += '      <img src="C.Elegans_%s.%s.png" alt="%s_%s" height="%s"/><br/>\n'%(cell,plane, cell, plane, height)
 
-    info2 += '   </td>\n\n'
+    info2 += '    </td>\n\n  </tr>\n\n'
 
-    info2 += '   <td><img src="../NeuronBlenderImaging/NeuronScreenshots/%s_side.png" alt="%s" height="%s"/><br/>\n'%(cell,cell, height)
-    info2 += '   <img src="../NeuronBlenderImaging/NeuronScreenshots/%s_front.png" alt="%s" height="%s"/><br/>\n'%(cell,cell,height)
-    info2 += '   <img src="../NeuronBlenderImaging/NeuronScreenshots/%s_top.png" alt="%s" height="%s"/></td></tr>\n\n'%(cell,cell, height)
     info2 += '</table>\n\n'
 
 
-info += '\n\n%s'%info2
+info = '%s\n\n%s'%(info[:-3], info2)
 
-
-f = open('README.md','w')
+fn = 'README.md'
+f = open(fn,'w')
 f.write(info)
 f.close()
+print('Written to:  %s'%fn)
